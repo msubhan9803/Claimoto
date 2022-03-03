302aecbdbe205644fffa6fac31d790edd3a52257
jehanxaibahmed <47818622+jehanxaibahmed@users.noreply.github.com>
2022-03-03 18:23:17 +0500

    Changes

--- src/components/Admin/Providers/ProviderAddModal.js
+++ src/components/Admin/Providers/ProviderAddModal.js
@@ -16,7 +16,7 @@ const ProviderAddModal = ({ openModal, toggleModal }) => {
     const { permissions } = useSelector(state => state.authReducer);
     let garage_actions = getAllowActions({ permissions, module_name: "PG" });
     let agency_actions = getAllowActions({ permissions, module_name: "PA" });
-    let car_agency_actions = getAllowActions({ permissions, module_name: "PC" });
+    let car_agency_actions = getAllowActions({ permissions, module_name: "PCA" });
     let surveyor_actions = getAllowActions({ permissions, module_name: "PS" });
 
 
