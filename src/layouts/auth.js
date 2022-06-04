6559b8108ad284dfb09638d23eb6deabc59572a1
jehanxaibahmed <47818622+jehanxaibahmed@users.noreply.github.com>
2022-06-04 14:04:53 +0500

    Changes

--- src/layouts/auth.js
+++ src/layouts/auth.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { AuthRoutes } from "../routes/auth";
 import { Route, Routes, } from "react-router-dom";
 import NotFound from "views/pages/404/404";
+import { getFCMToken } from "config/firebase/firebase";
+
+
+
 export default function AuthRoute() {
+
+    const [isTokenFound, setTokenFound] = useState(false);
+    getFCMToken(setTokenFound);
+
 
     let routes = AuthRoutes();
 
