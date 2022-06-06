43efe9e816f5da3c41a79e37c93f76d15f414c3d
jehanxaibahmed <47818622+jehanxaibahmed@users.noreply.github.com>
2022-06-06 12:31:09 +0500

    Changes

--- src/variables/index.js
+++ src/variables/index.js
@@ -1,3 +1,3 @@
 export const localStorageVarible = "claimoto_auth_key";
-export const ENVIRONMENT = "DEVELOPMENT";
-// export const ENVIRONMENT = "LOCAL";
+// export const ENVIRONMENT = "DEVELOPMENT";
+export const ENVIRONMENT = "LOCAL";
