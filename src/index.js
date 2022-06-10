bf02506c104db7530ae941e3a14d8662eb2621e2
msubhan33 <msubhan9803@gmail.com>
2022-06-10 12:28:11 +0500

    jahanzaib-dev new code notification merged

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import AdminLayout from "layouts/admin";
@@ -8,17 +8,43 @@ import { Provider } from "react-redux";
 import { store, persistor } from "store/store";
 import { PersistGate } from 'redux-persist/integration/react'
 import NotFound from "views/pages/404/404";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // import 'loader'
 // import 'assets/js/main';
-import 'font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 import history from "utils/history";
 import "config/firebase/firebase";
+import { ENVIRONMENT } from "variables";
 require("jquery-nice-select");
+
+
+
+if (ENVIRONMENT !== "DEVELOPMENT") {
+  navigator.serviceWorker.addEventListener("notificationclick", (message) => {
+    console.log(message);
+  });
+  navigator.serviceWorker.addEventListener("message", (message) => {
+    var {data, notification} = message.data;
+    toast(({ closeToast }) => <div>  <h6>{notification.title}</h6><small>{notification.body}</small>  </div>, {
+      onClick:() => _navigateToClaim(data.redirect_url)
+    });
+  });
+}
+
+
+const _navigateToClaim = (redirect_url) => {
+  console.log(redirect_url);
+}
+
+
+
 
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <ToastContainer />
       <Router history={history}>
         <Routes>
           <Route path="/*" element={<AuthLayout />} />
